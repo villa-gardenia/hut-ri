7cc46f69a7ab0378f883b74539b3eb84557565ae
dedenbangkit <mail@dedenbangkit.com>
2025-06-19 00:59:21 +0700

    [#2] Try new base path

--- src/next.config.js
+++ src/next.config.js
@@ -4,7 +4,6 @@ const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-
   basePath: isProd ? "/hut-ri" : "",
   assetPrefix: isProd ? "/hut-ri/" : "",
   output: "export",
