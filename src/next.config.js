f2d9429d7af86ae652c27b664dcc1bb141a869ab
dedenbangkit <mail@dedenbangkit.com>
2025-06-18 23:18:00 +0700

    [#2] Fix deployment issue

--- src/next.config.js
+++ src/next.config.js
@@ -2,10 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-
-  // Untuk deployment ke https://villa-gardenia.github.io/hut-ri
   basePath: "/hut-ri",
-
-  // Output standalone agar kompatibel dengan Docker
   output: "standalone",
 };
